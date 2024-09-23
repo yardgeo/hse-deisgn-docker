2da9c28a33a90580527b06af77c5623ce486c0b8
Georgii Iardukhin <43379766+yardgeo@users.noreply.github.com>
2024-09-23 23:22:02 +0300

    Update start_all.sh

--- k8s-files/start_all.sh
+++ k8s-files/start_all.sh
@@ -3,7 +3,8 @@ cd db
 kubectl apply -f pvc.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
-
+echo "App was created"
+sleep 10
 # starting app
 cd ../app
 kubectl apply -f deployment.yaml
