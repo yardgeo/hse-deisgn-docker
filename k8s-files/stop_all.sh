adf7b64b75767984d2a8b6ec784390130a147b2f
Georgii Iardukhin <43379766+yardgeo@users.noreply.github.com>
2024-09-23 23:05:15 +0300

    Create stop_all.sh

---
+++ k8s-files/stop_all.sh
@@ -0,0 +1,10 @@
+# deleting app
+cd app
+kubectl delete -f service.yaml
+kubectl delete -f deployment.yaml
+
+# deleting db
+cd db
+kubectl delete -f service.yaml
+kubectl delete -f deployment.yaml
+kubectl delete -f pvc.yaml
